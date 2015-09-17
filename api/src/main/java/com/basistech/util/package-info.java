62b9299954bfe18363cc644455178370a22f153f
Benson Margulies <benson@basistech.com>
2015-09-17 11:28:43 -0400

    actually put the annotations to work.

---
+++ api/src/main/java/com/basistech/util/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * * This data and information is proprietary to, and a valuable trade secret
+ * * of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ * * and may only be used as permitted under the license agreement under which
+ * * it has been distributed, and in no other way.
+ * *
+ * * Copyright (c) 2015 Basis Technology Corporation All rights reserved.
+ * *
+ * * The technical data and information provided herein are provided with
+ * * `limited rights', and the computer software provided herein is provided
+ * * with `restricted rights' as those terms are defined in DAR and ASPR
+ * * 7-104.9(a).
+ * <p/>
+ * Created by benson on 9/17/15.
+ */
+
+/**
+ * The classes in this package are less-frequently used parts
+ * of the API, including some Spring support.
+ */
+@RosetteSystemBundlePackage
+package com.basistech.util;
+
+import com.basistech.rosette.RosetteSystemBundlePackage;
