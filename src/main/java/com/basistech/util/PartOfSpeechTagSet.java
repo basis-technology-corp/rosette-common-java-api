b04aa624843b16248d978481c832ed4d68f7eec5
Benson Margulies <benson@basistech.com>
2015-12-23 10:13:44 -0500

    RCB-237: Enum for part of speech tag set.

---
+++ src/main/java/com/basistech/util/PartOfSpeechTagSet.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * * This data and information is proprietary to, and a valuable trade secret
+ * * of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ * * and may only be used as permitted under the license agreement under which
+ * * it has been distributed, and in no other way.
+ * *
+ * * Copyright (c) 2015 Basis Technology Corporation All rights reserved.
+ * *
+ * * The technical data and information provided herein are provided with
+ * * `limited rights', and the computer software provided herein is provided
+ * * with `restricted rights' as those terms are defined in DAR and ASPR
+ * * 7-104.9(a).
+ ******************************************************************************/
+
+package com.basistech.util;
+
+/**
+ * This enum names different tag sets for part of speech tags.
+ */
+public enum PartOfSpeechTagSet {
+    /** Basis Technology's fine-granularity part of speech tags. **/
+    BASIS,
+    /** The 16 tag 'Universal POS Tag' set.
+     * See <a href="http://universaldependencies.github.io/docs/u/pos/">Universal POS Tags</a>**/
+    UPT16
+}
