00afb9f1bf308d51fecd79ecb69a5ba6c6d707df
David Corbett <dcorbett@basistech.com>
2022-10-25 11:01:01 -0400

    COMN-289: Improve the SonarQube report

--- api/src/test/java/com/basistech/util/TextDomainTest.java
+++ api/src/test/java/com/basistech/util/TextDomainTest.java
@@ -13,6 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+
 package com.basistech.util;
 
 import org.junit.Before;
@@ -58,14 +59,14 @@ public class TextDomainTest {
         TextDomain tdArabicByHand = new TextDomain(ISO15924.Arab, LanguageCode.ARABIC,
                                                    TransliterationScheme.UNKNOWN);
         TextDomain tdArabicNative = new TextDomain(LanguageCode.ARABIC);
-        assertTrue(tdArabicByHand.equals(tdArabicNative));
+        assertEquals(tdArabicByHand, tdArabicNative);
 
     }
 
     @Test
     public void testhashCode() {
-        assertTrue(td1.hashCode() == td2.hashCode());
-        assertTrue(td2.hashCode() == td3.hashCode());
+        assertEquals(td1.hashCode(), td2.hashCode());
+        assertEquals(td2.hashCode(), td3.hashCode());
         assertNotEquals(td1.hashCode(), tdDefault.hashCode());
     }
 
@@ -75,14 +76,13 @@ public class TextDomainTest {
         // we're debugging:
         TextDomain prs = new TextDomain(LanguageCode.DARI);
         TextDomain pes = new TextDomain(LanguageCode.WESTERN_FARSI);
-        assertFalse(prs.toString().equals(pes.toString()));
+        assertNotEquals(prs.toString(), pes.toString());
         // test that we don't crash on some null cases.
         TextDomain ntd = new TextDomain(null, null, null);
-        // some silly asserts to try to shut up intelliJ.
         assertNotNull(ntd.toString());
-        assertNotNull(ntd.hashCode());
+        ntd.hashCode();
         assertEquals(ntd, ntd);
-        assertNotNull(ntd.equals(prs));
-        assertNotNull(pes.equals(ntd));
+        ntd.equals(prs);
+        pes.equals(ntd);
     }
 }
